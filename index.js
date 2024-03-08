05982127d26926200e9b95d4d26d93cf8b0e7893
unknown <redakgaming@gmail.com>
2024-03-09 01:29:20 +0530

    update index

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-const readline = require("readline-sync");
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
-require('dotenv').config();
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const cors = require("cors");
-const { log } = require("console");
+const fetch = require("node-fetch");
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Distance and Time API',
-            version: '1.0.0',
-            description: 'API for calculating distance using Google Maps API',
-        },
-        servers: [
-            {
-                url: 'http://localhost:5000/'
-            }
-        ]
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Distance and Time API',
+      version: '1.0.0',
+      description: 'API for calculating distance and Time using Google Maps API',
     },
-    apis: ['index.js'],
+  },
+  apis: ['index.js'], // Assuming your API routes are defined in 'index.js'
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 app.use('/distance', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 /**
  * @swagger
@@ -55,30 +49,30 @@ app.use('/distance', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
  *         description: Successful response
  *         content:
  *           application/json:
- *                  example:
- *                        distance: '100 km'
- *                        time: '38 mins'
+ *             example:
+ *               distance: '100 km'
+ *               time: '38 mins'
  */
 
 app.get("/find", async (req, res) => {
-    // access the location from body or use default location which in this case hansi and hisar
-    const origin = req.query.origin;
-    const destination = req.query.destination;
-    const apikey = process.env.apikey;
-    // fetch google map api
-    const fetchDistance = await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=${apikey}`)
-    // convert data into json format
+  const origin = req.query.origin;
+  const destination = req.query.destination;
+  const apiKey = process.env.apikey;
+
+  try {
+    const fetchDistance = await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=${apiKey}`);
     const data = await fetchDistance.json();
-    // get location and time
     const distance = data.rows[0].elements[0].distance.text;
     const time = data.rows[0].elements[0].duration.text;
     res.send({
-        distance,
-        time
+      distance,
+      time
     });
-})
-
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log("connected");
-})
+  console.log(`Server is running on ${PORT}`);
+});
